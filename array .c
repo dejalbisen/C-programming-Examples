bf042ae22a1eb5443a928adbe364a2f0edba37df
dejalbisen <57594197+dejalbisen@users.noreply.github.com>
2020-10-20 13:46:53 +0530

    Add files via upload

---
+++ array .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[10];
+	int i;
+	for(i=0;i<=9;i++)
+	{
+		printf("enter no.:");
+		scanf("%d",&arr[i]);
+			}
+for(i=0;i<=9;i++)
+{
+	printf("/n%d",display);
+}
+getch();
+}
