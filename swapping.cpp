a8ad9672903eba27c8969dd4d6c03a5653b8f467
dejalbisen <57594197+dejalbisen@users.noreply.github.com>
2020-10-25 20:46:16 +0530

    Add files via upload

---
+++ swapping.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=4, b=8, temp;
+	cout<<"before swapping"<<endl;
+	cout<<"a="<<a<<"b="<<b<<endl;
+	temp=a;
+	a=b;
+	b=temp;
+	cout<<"after swapping"<<endl;
+	cout<<"a="<<a<<"b="<<b<<endl;
+	return 0;
+	
+}
