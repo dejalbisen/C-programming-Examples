86a1c45d730f38020d522061c9c5443cf3d9a6e2
dejalbisen <57594197+dejalbisen@users.noreply.github.com>
2021-05-22 11:29:38 +0530

    Add files via upload

---
+++ fabonacci series.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,t1=0,t2=1,nextTerm=0;
+	cout<<"enter the number of terms:";
+	cin>>n;
+	cout<<"fibonacci series:";
+	for(int i=1;i<=n;++i)
+	{
+	if(i==1)
+	{
+		cout<<" "<<t1;
+		continue;
+	}
+	nextTerm=t1+12;
+	t1=t2;
+	t2=nextTerm;
+	cout<<nextTerm<<" ";
+	}
+	return 0;
+}
---
+++ print no..by user.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"enter the number: \n";
+	cin>>num;
+	cout<<"the number entered by user is:"<<num;
+	return 0;
+}
